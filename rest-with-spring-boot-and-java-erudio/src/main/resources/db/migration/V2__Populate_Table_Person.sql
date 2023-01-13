cdbe8cc40c2b300192ee95903822ac72ca8e9a8a
Kaio <kgalvo47@gmail.com>
2023-01-13 15:21:03 -0300

    migrações com flyway

---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(2, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+	(3, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vince'),
+	(5, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+	(7, 'Kentucky - USA', 'Mugammad', 'Male', 'Ali '),
+	(8, 'Kentucky - USA', 'Mugammad', 'Male', 'Ali '),
+	(9, 'Smiljan - Croatia', 'Nikola', 'Male', 'Telas ');
+
+
