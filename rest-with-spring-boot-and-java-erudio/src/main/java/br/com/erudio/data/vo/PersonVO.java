28afaa913ea47e1670a32c53e2dad481a0cb16a4
Kaio <kgalvo47@gmail.com>
2023-01-13 15:44:24 -0300

    manipulação de json

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/PersonVO.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/PersonVO.java
@@ -3,20 +3,24 @@ package br.com.erudio.data.vo;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+@JsonPropertyOrder({"id", "address","firstName","lastName","gender"})
 public class PersonVO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private long id;
-	
+	@JsonProperty("first_name")
 	private String firstName;
 	
-	
+	@JsonProperty("last_name")
 	private String lastName;
 	
 	
 	private String address;
 	
-	
+	@JsonIgnore
 	private String gender;
 	
 	public PersonVO() {
